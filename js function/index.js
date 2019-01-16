fbdeac2f4efbcbec94f800ca821176fbffe6d122
talentamaulana10 <tatamf.id@gmail.com>
2019-01-16 03:15:50 +0000

    js function

---
+++ js function/index.js
@@ -0,0 +1,9 @@
+function binus(name, lastName) {
+    console.log('Hay ' + name + ' ' + lastName);
+  function square(number) {
+    return number * number;
+  }
+  
+  binus('talenta', 'fighan!!');
+  let number = square(2);
+  console.log(number);
