1230e9039244723b98d6c674a05e990c8ac7aa6b
talentamaulana10 <tatamf.id@gmail.com>
2019-01-16 10:59:41 +0000

    Exercise 2- Factory and Constructor Functions

---
+++ Exercise 2- Factory and Constructor Functions/index.js
@@ -0,0 +1,14 @@
+function createAddress(street, city, zipCode) {
+    return {
+      street: 'a',
+      city: 'b',
+      zipCode: 'c'
+    };
+  }
+  
+  function showAddress(street, city, zipCode) {
+    this.street = street;
+    this.city = city;
+    this.zipCode = zipCode;
+  }
+  
