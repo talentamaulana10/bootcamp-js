1269110ab3e7b69f948711a40d6ae39218bbb532
talentamaulana10 <tatamf.id@gmail.com>
2019-01-16 03:17:38 +0000

    aritmatika operator

---
+++ aritmatika operator/index.js
@@ -0,0 +1,10 @@
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+let x = 10;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
